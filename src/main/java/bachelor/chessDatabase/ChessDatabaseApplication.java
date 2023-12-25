ba1347610605f2bec90ee1a0822b04502baeec83
Hrithik Sharma <e12023164@student.tuwien.ac.at>
2023-12-25 23:05:53 +0100

    Initial commit

---
+++ src/main/java/bachelor/chessDatabase/ChessDatabaseApplication.java
@@ -0,0 +1,13 @@
+package bachelor.chessDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bachelor/chessDatabase/ChessDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package bachelor.chessDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
