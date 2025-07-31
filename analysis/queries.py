b0d7a2b436f7336da124ccc726a924ef126c0b90
Hrithik Sharma <e12023164@student.tuwien.ac.at>
2025-08-01 00:21:20 +0200

    Added remaining queries for analysis (older)

--- analysis/queries.py
+++ analysis/queries.py
@@ -5,6 +5,10 @@ import os
 URI = "bolt://localhost:7687"
 AUTH = ("neo4j", "chesschess")
 
+#
+# PATHS
+#
+
 data_dir = "data"
 opening_outfile_name = os.path.join(data_dir,"world_rapid_2024_opening_occurences.csv")
 relative_branching_outfile_name = os.path.join(data_dir,"world_rapid_2024_relative_branching.csv")
@@ -12,6 +16,11 @@ absolute_branching_outfile_name = os.path.join(data_dir,"world_rapid_2024_absolu
 existing_games_path = os.path.join(data_dir,"existing_games_up_to_depth.csv")
 opening_total_path = os.path.join(data_dir,"opening_total_positions_upto_depth.csv")
 unique_openings_path = os.path.join(data_dir,"opening_unique_positions_upto_depth.csv")
+most_played_opening_depth = os.path.join(data_dir,"most_played_opening_upto_depth.csv")
+
+#
+# OPENING QUERIES
+#
 
 # This method executes a query which retrieves the count of games for each opening and writes the results to a CSV file.
 def get_standard_opening_count(session, csv_name=opening_outfile_name):
@@ -46,7 +55,96 @@ def get_Length_of_longest_opening(session):
     result = session.run(get_longest_opening_query)
     for record in result:
         print(f" Opening Name: {record['opening_name']}, Move Count: {record['move']}, Ply Count: {record['ply']}")
- 
+
+def get_total_opening_positions_upto_depth(session, depth=None, outfile_name=opening_total_path, save=False):
+    if depth is None:
+        raise ValueError("Depth must be specified and cannot be None.")
+    
+    get_opening_positions_query = (
+        f"UNWIND range(1,{int(depth)}) AS depth "
+        "OPTIONAL MATCH (g:Game)-[p:position]->(pos:Opening) "
+        "WHERE p.ply = depth "
+        "RETURN depth, count(pos) AS opening_count "
+    )
+    
+    result = session.run(get_opening_positions_query)
+        
+    if save:
+        with open(outfile_name, "w", encoding="utf-8") as file:
+            file.write("depth\topening_count\n")
+            for record in result:
+                print(f"Depth: {record['depth']}, Opening Count: {record['opening_count']}")
+                file.write(f"{record['depth']}\t{record['opening_count']}\n")
+                
+def get_games_with_longest_opening(session):
+    get_games_query = (
+        "MATCH (g:Game)-[p:position]->(pos:Opening) "
+        "MATCH (g) -[:result_of_game]->(res) "
+        "WITH g, p.ply AS move_count, pos.name AS opening_name, res.fen AS result "
+        "ORDER BY move_count DESC "
+        "RETURN g.gameNumber as game, move_count, opening_name, result "
+        "LIMIT 3"
+    )
+    
+    result = session.run(get_games_query)
+    for record in result:
+        print(f"Game ID: {record['game']}, Move Count: {record['move_count']}, Opening Name: {record['opening_name']}, Result: {record['result']}")                
+
+def get_unique_opening_positions_upto_depth(session, depth=None, outfile_name=unique_openings_path, save=False):
+    if depth is None:
+        raise ValueError("Depth must be specified and cannot be None.")
+    
+    get_opening_positions_query = (
+        f"UNWIND range(1,{int(depth)}) AS depth "
+        "OPTIONAL MATCH (g:Game)-[p:position]->(pos:Opening) "
+        "WHERE p.ply = depth "
+        "RETURN depth, count(DISTINCT pos) AS opening_count "
+    )
+    
+    result = session.run(get_opening_positions_query)
+        
+    if save:
+        with open(outfile_name, "w", encoding="utf-8") as file:
+            file.write("depth\topening_count\n")
+            for record in result:
+                print(f"Depth: {record['depth']}, Opening Count: {record['opening_count']}")
+                file.write(f"{record['depth']}\t{record['opening_count']}\n")
+
+def get_top_openings_per_depth(session, depth=None, outfile_name=most_played_opening_depth, save=False):
+    if depth is None:
+        raise ValueError("Depth must be specified and cannot be None.")
+    get_top_openings_query = (
+        f"UNWIND range(1,{int(depth)}) AS depth "
+        "MATCH (g:Game)-[p:position]->(pos:Opening) "
+        "WHERE p.ply = depth "
+        "WITH pos.name AS opening_name, count(DISTINCT g) AS game_count, depth "
+        "RETURN opening_name, game_count, depth "
+        "ORDER BY game_count DESC "
+    )
+    result = session.run(get_top_openings_query)
+    result_copy = list(result)
+    openings_depth = []
+    for i in range(depth):
+        cur_opening = ""
+        cur_count = 0
+        for record in result_copy:
+            tmp_depth = int(record['depth'])
+            tmp_count = int(record['game_count'])
+            if tmp_depth == i + 1:
+                if tmp_count > cur_count:
+                    cur_opening = record['opening_name']
+                    cur_count = tmp_count
+        openings_depth.append((i+1, cur_opening, cur_count))
+    if save:
+        with open(outfile_name, "w", encoding="utf-8") as file:
+            file.write("opening_name\tdepth\tgame_count\n")
+            for (depth, opening_name, game_count) in openings_depth:
+                file.write(f"{opening_name}\t{depth}\t{game_count}\n")
+
+#
+# BRANCHING FACTOR QUERIES
+#
+
 # This method executes a query which retrieves the abosolute number of positions reached after x moves.       
 def get_absolute_branching_factor_upto_depth(session, depth=None, outfile_name=absolute_branching_outfile_name, save=False):
     if depth is None:
@@ -121,7 +219,11 @@ def get_relative_branching_factor_upto_depth(session, depth=None, outfile_name=r
             file.write("depth\trelative_branching_factor\n")
             for depth, branching_factor in branching.items():
                 file.write(f"{depth}\t{branching_factor}\n")
-    
+
+#
+# (ACTIVE) GAME RELATED QUERIES
+#
+   
 def get_existing_games_up_to_depth(session, depth=None, outfile_name=existing_games_path, save=False):
     if depth is None:
         raise ValueError("Depth must be specified and cannot be None.")
@@ -143,45 +245,6 @@ def get_existing_games_up_to_depth(session, depth=None, outfile_name=existing_ga
                 print(f"Depth: {record['depth']}, Number of Games: {record['game_count']}")
                 file.write(f"{record['depth']}\t{record['game_count']}\n")
                 
-def get_total_opening_positions_upto_depth(session, depth=None, outfile_name=opening_total_path, save=False):
-    if depth is None:
-        raise ValueError("Depth must be specified and cannot be None.")
-    
-    get_opening_positions_query = (
-        f"UNWIND range(1,{int(depth)}) AS depth "
-        "OPTIONAL MATCH (g:Game)-[p:position]->(pos:Opening) "
-        "WHERE p.ply = depth "
-        "RETURN depth, count(pos) AS opening_count "
-    )
-    
-    result = session.run(get_opening_positions_query)
-        
-    if save:
-        with open(outfile_name, "w", encoding="utf-8") as file:
-            file.write("depth\topening_count\n")
-            for record in result:
-                print(f"Depth: {record['depth']}, Opening Count: {record['opening_count']}")
-                file.write(f"{record['depth']}\t{record['opening_count']}\n")                
-
-def get_unique_opening_positions_upto_depth(session, depth=None, outfile_name=unique_openings_path, save=False):
-    if depth is None:
-        raise ValueError("Depth must be specified and cannot be None.")
-    
-    get_opening_positions_query = (
-        f"UNWIND range(1,{int(depth)}) AS depth "
-        "OPTIONAL MATCH (g:Game)-[p:position]->(pos:Opening) "
-        "WHERE p.ply = depth "
-        "RETURN depth, count(DISTINCT pos) AS opening_count "
-    )
-    
-    result = session.run(get_opening_positions_query)
-        
-    if save:
-        with open(outfile_name, "w", encoding="utf-8") as file:
-            file.write("depth\topening_count\n")
-            for record in result:
-                print(f"Depth: {record['depth']}, Opening Count: {record['opening_count']}")
-                file.write(f"{record['depth']}\t{record['opening_count']}\n")
 
 # This method executes a query which retrieves the number of games in the database.
 def get_number_of_games(session):
@@ -194,6 +257,10 @@ def get_number_of_games(session):
     for record in result:
         print(f"Number of games: {record['game_count']}")
     return record['game_count']
+
+#
+# WRAPPER FUNCTION THAT CALLS THE QUERIES
+#
     
 # This method executes the method (query) provided in the argument
 def execute_query(method, **kwargs):
@@ -205,9 +272,15 @@ def execute_query(method, **kwargs):
     if output:
         return output
 
+
+#
+# MAIN FUNCTION
+#
+
 def main():
-    execute_query(get_unique_opening_positions_upto_depth, depth=55, save=True)	
-    execute_query(get_total_opening_positions_upto_depth, depth=55, save=True)
+    #execute_query(get_unique_opening_positions_upto_depth, depth=55, save=True)	
+    #execute_query(get_total_opening_positions_upto_depth, depth=55, save=True)
+    execute_query(get_top_openings_per_depth, depth=15, save=True)
     
 if __name__ == "__main__":
     main()
--- analysis/visualization.py
+++ analysis/visualization.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import powerlaw
+from scipy.stats import linregress
 import os
+
+#
+# LOADING DATA
+#
 
 def load_opening_data(file_path):
     """
@@ -15,6 +21,18 @@ def load_opening_data(file_path):
     df = df[df['Played'] > 0]
     print(len(df), "openings loaded from file:", file_path)
     return df
+
+def load_opening_depth_data(unique=False, cutoff=30):
+    if unique:
+        cols = ["Depth", "Opening Count"]
+        file_path = os.path.join("data","opening_unique_positions_upto_depth.csv")
+    else:
+        cols = ["Depth", "Opening Count"]
+        file_path = os.path.join("data","opening_total_positions_upto_depth.csv")
+    df = pd.read_csv(file_path, sep="\t", header=0, names=cols)
+    df['Depth'] = df['Depth'].astype(int)
+    df['Opening Count'] = df['Opening Count'].astype(int)
+    return df.head(cutoff)
 
 def load_perft(cutoff=10):
     file_path = os.path.join("data","perft.csv")
@@ -42,27 +60,79 @@ def load_branching_data(absolute=True, cutoff=60):
         df['Relative Branching Factor'] = df['Relative Branching Factor'].astype(float)
     return df.head(cutoff)
 
-def visualize_opening(file_path):
+#
+# VISUALIZATION FUNCTIONS
+#
+
+#
+# OPENINGS
+#
+
+def visualize_opening(file_path, eco=False):
     """
     Visualize the data from the file. The file should contain the name of the openeings, how often they were played and their resulting ranking
     """
     df = load_opening_data(file_path)
-    ylabel = 'Number of Games containing Opening'
+    ylabel = 'Frequency'
 
+    if eco:
+        eco_played  = df.groupby('ECO')['Played'].sum().sort_values(ascending=False)
+        eco_df = eco_played.reset_index()
+        eco_df['Ranking'] = eco_df['Played'].rank(method='dense', ascending=False).astype(int)
+        print(eco_df)
+        df = eco_df
     plt.figure(figsize=(10, 6))
-    plt.loglog(df['Ranking'], df['Played'], color='darkblue', label='Log-log Plot of Openings Played')
+    plt.loglog(df['Ranking'], df['Played'], color='darkblue', label='Openings played in the tournament')
 
     x_ref = np.linspace(df['Ranking'].min(), df['Ranking'].max(), len(df))
     y_ref = int(df.iloc[0]["Played"]) / x_ref
     plt.loglog(x_ref, y_ref, 'r--', label='Reference line')
 
     plt.ylabel(ylabel)
-    plt.xlabel('Openings from ECO database')
+    plt.xlabel('Ranking')
     plt.title('Power Scale of Openings Played')
     plt.xticks(fontsize=6, rotation=90)
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+def visualize_opening_unique_per_depth(cutoff=30):
+    """
+    Visualize the number of unique openings per depth.
+    """
+    
+    df = load_opening_depth_data(unique=True, cutoff=cutoff)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Depth'], df['Opening Count'], marker='o', color='purple', label='Openings')
+    plt.xlabel('Ply')
+    plt.ylabel('Unique Openings')
+    plt.title('Unique Openings per Depth (World Rapid 2024)')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+def visualize_opening_total_per_depth(cutoff=30):
+    """
+    Visualize the total number of openings per depth.
+    """
+    
+    df = load_opening_depth_data(unique=False, cutoff=cutoff)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Depth'], df['Opening Count'], marker='o', color='orange', label='Games')
+    plt.xlabel('Ply')
+    plt.ylabel('Games')
+    plt.title('Number of Games following Opening Theory (World Rapid 2024)')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+#
+# BRANCHING FACTOR
+#
 
 def visualize_relative_branching(cutoff=60):
     """
@@ -79,7 +149,7 @@ def visualize_relative_branching(cutoff=60):
     plt.tight_layout()
     plt.show()
 
-def visualize_absolute_branching(cutoff=60, with_games=False):
+def visualize_absolute_branching(cutoff=60, with_games=False, with_openings=False):
     """
     Visualize the absolute branching factor data. Optionally, add the amount of active games after each ply.
     """
@@ -87,12 +157,17 @@ def visualize_absolute_branching(cutoff=60, with_games=False):
     plt.figure(figsize=(10, 6))
     plt.plot(df['Depth'], df['Absolute Branching Factor'], marker='o', color='blue', label='Absolute Branching Factor')
     if with_games:
-        file_path = "existing_games_up_to_depth.csv"
+        file_path = os.path.join("data","existing_games_up_to_depth.csv")
         games_until_depth = pd.read_csv(file_path, sep="\t", header=0, names=["Depth", "Games"])
         games_until_depth['Depth'] = games_until_depth['Depth'].astype(int)
         games_until_depth = games_until_depth.head(cutoff)
         plt.plot(games_until_depth['Depth'], games_until_depth['Games'], marker='x', color='red', label='Active Games')
-        print("OK")
+    if with_openings:
+        file_path = os.path.join("data","opening_total_positions_upto_depth.csv")
+        openings_until_depth = pd.read_csv(file_path, sep="\t", header=0, names=["Depth", "Opening Count"])
+        openings_until_depth['Depth'] = openings_until_depth['Depth'].astype(int)
+        openings_until_depth = openings_until_depth.head(cutoff)
+        plt.plot(openings_until_depth['Depth'], openings_until_depth['Opening Count'], marker='x', color='orange', label='Opening Positions')
     plt.xlabel('Ply')
     plt.ylabel('Total Distinct Positions')
     plt.title('Absolute Branching Factor (World Rapid 2024)')
@@ -140,9 +215,46 @@ def visualize_absolute_branching_versus_perft(cutoff=10):
     plt.tight_layout()
     plt.show()
     
+#    
+# EXTRA   
+#    
+    
+def visualize_and_compute_power_law_openings(opening = "world_rapid_2024_opening_occurences.csv"):
+    df = load_opening_data(opening)
+    print(df['Played'])
+    fit = powerlaw.Fit(df['Played'], xmin=3)
+    alpha = fit.power_law.alpha
+    xmin = fit.power_law.xmin
+    print(f"Alpha: {alpha}")
+    print(f"Xmin: {xmin}")
+    
+    rank = df['Ranking']
+    frquency = df['Played']
+    log_rank = np.log10(rank)
+    log_frequency = np.log(frquency)
+    slope, intercept, r_value, p_value, std_err = linregress(log_rank, log_frequency)
+    zipf_exponent = -slope  # because f(r) ∝ 1 / r^s
+    print(f"Estimated Zipf exponent: {zipf_exponent:.3f}")
+    print(f"R²: {r_value**2:.3f}")
+    
+    plt.figure(figsize=(10, 6))
+    fig = fit.plot_pdf(color='b', linewidth=2, label='Opening Occurrences')
+    fit.power_law.plot_pdf(color='r', linestyle='--', label='Power Law Fit')
+    plt.xlabel('Openings')
+    plt.ylabel('Popularity')
+    plt.title('Power Law Distribution of Openings')
+    plt.legend()
+    plt.show()
+    
+#
+# MAIN
+#
     
 if __name__ == "__main__":
     data_path = "data"
-    opening_file = os.join.path(data_path,"world_rapid_2024_opening_occurences.csv")
-    visualize_absolute_branching_versus_perft(cutoff=6)
-    #visualize_relative_branching_2(cutoff=20)
+    opening_file = os.path.join(data_path,"world_rapid_2024_opening_occurences.csv")
+    #visualize_opening_unique_per_depth()
+    #visualize_absolute_branching(with_games=True, with_openings=True)
+    #visualize_and_compute_power_law_openings()
+    visualize_opening("world_rapid_2024_opening_occurences.csv")
+    
